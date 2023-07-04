85fb8f6f5d2ba2da6a8fb603911e91659a119e70
Stu Rob <git@sturob.com>
2023-07-04 17:38:40 -0400

    always be next to the edge crossed when transitioning sessions

--- scripts/down.sh
+++ scripts/down.sh
@@ -4,6 +4,11 @@ is_bottom=$(tmux display-message -p -F "#{pane_at_bottom}")
 
 if [ $is_bottom -eq 1 ]; then
 	tmux switch-client -n
+	
+	top_edge_pane=$(
+		tmux list-panes -F '#{pane_id} #{pane_at_top}' | awk "/ 1$/" | head -n 1 | awk '{print $1}'
+	)
+	tmux select-pane -t $top_edge_pane
 else
 	tmux select-pane -D
 fi
--- scripts/up.sh
+++ scripts/up.sh
@@ -2,8 +2,13 @@
 
 lines_from_top=$(tmux display-message -p -F "#{pane_top}")
 
+
 if [ $lines_from_top -eq 0 ]; then
 	tmux switch-client -p
+	bottom_edge_pane=$(
+		tmux list-panes -F '#{pane_id} #{pane_at_bottom}' | awk "/ 1$/" | head -n 1 | awk '{print $1}'
+	)
+	tmux select-pane -t $bottom_edge_pane
 else
 	tmux select-pane -U
 fi
