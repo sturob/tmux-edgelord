402042a0b8d56cdd070440c28aaa93d8cd55456a
Stu Rob <git@sturob.com>
2023-07-04 16:35:58 -0400

    always be next to the edge crossed when transitioning windows

--- scripts/left.sh
+++ scripts/left.sh
@@ -4,6 +4,10 @@ is_left=$(tmux display-message -p -F "#{pane_at_left}")
 
 if [ $is_left -eq 1 ]; then
 	tmux prev
+	right_edge_pane=$(
+ 		tmux list-panes -F '#{pane_id} #{pane_at_right}' | awk "/ 1$/" | head -n 1 | awk '{print $1}'
+ 	)
+	tmux select-pane -t $right_edge_pane
 else
 	tmux select-pane -L
 fi
--- scripts/right.sh
+++ scripts/right.sh
@@ -4,6 +4,10 @@ is_right=$(tmux display-message -p -F "#{pane_at_right}")
 
 if [ $is_right -eq 1 ]; then
 	tmux next
+	left_pane=$(
+ 		tmux list-panes -F '#{pane_id} #{pane_at_left}' | awk "/ 1$/" | head -n 1 | awk '{print $1}'
+ 	)
+	tmux select-pane -t $left_pane
 else
 	tmux select-pane -R
 fi
